15fb3de2a210b7c136a17119e4f908225e36be3d
Nadav Dan <nadav.prod@gmail.com>
2026-05-07 22:18:12 +0300

    initial loading of the elf header

--- include/elf.h
+++ include/elf.h
@@ -1,8 +1,46 @@
 #include <stdint.h>
-#define ELF_MAGIC_NUMBER_LENGTH 4
+#define ELF_IDENT_MAGIC_NUMBER_LENGTH 4
+#define ELF_IDENT_PADDING_LENGTH 7
 
 typedef struct {
-  unsigned char magicnumber[ELF_MAGIC_NUMBER_LENGTH];
+  uint8_t magicnumber[ELF_IDENT_MAGIC_NUMBER_LENGTH];
   uint8_t bitformat;
   uint8_t endianness;
-} elfhead_t;
+  uint8_t version;
+  uint8_t osabi;
+  uint8_t osabiversion;
+  uint8_t padding[ELF_IDENT_PADDING_LENGTH];
+} __attribute__((packed)) elfheadident_t;
+
+typedef struct {
+  uint16_t filetype;
+  uint16_t machine;
+  uint32_t version;
+  uint64_t entry;
+  uint64_t programheader;
+  uint64_t sectionheader;
+  uint32_t flags;
+  uint16_t headersize;
+  uint16_t programheadersize;
+  uint16_t programheadernum;
+  uint16_t sectionheadersize;
+  uint16_t sectionheadernum;
+  uint16_t namessectionindex;
+} __attribute__((packed)) elfhead_t_64;
+
+typedef struct {
+  elfheadident_t ident;
+  uint16_t filetype;
+  uint16_t machine;
+  uint32_t version;
+  uint32_t entry;
+  uint32_t programheader;
+  uint32_t sectionheader;
+  uint32_t flags;
+  uint16_t headersize;
+  uint16_t programheadersize;
+  uint16_t programheadernum;
+  uint16_t sectionheadersize;
+  uint16_t sectionheadernum;
+  uint16_t namessectionindex;
+} __attribute__((packed)) elfhead_t_32;
--- src/sandbox.c
+++ src/sandbox.c
@@ -1,9 +1,12 @@
 #include "elf.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
   FILE *fptr;
-  elfhead_t elfheader;
+  elfheadident_t elfident;
+  void *elfheader;
+  int ret;
 
   printf("Sandbox start\n");
   fptr = fopen("./bins/program", "rb");
@@ -13,15 +16,38 @@ int main() {
     return 1;
   }
 
-  int ret = fread(&elfheader, sizeof(elfhead_t), 1, fptr);
+  // TODO: read based on endianness.
+  ret = fread(&elfident, sizeof(elfheadident_t), 1, fptr);
   printf("reaturned ret: %d\n", ret);
 
-  printf("Elf file header:\n");
-  printf("magicnumber: %s\n", elfheader.magicnumber);
-  printf("bitformat: %d\n", elfheader.bitformat);
-  printf("endianness: %d\n", elfheader.endianness);
+  char headeris64 = elfident.bitformat == 2;
+  size_t headallocsize =
+      headeris64 ? sizeof(elfhead_t_64) : sizeof(elfhead_t_32);
+  printf("Alloc size: %zu\n", headallocsize);
+  elfheader = malloc(headallocsize);
+  ret = fread(elfheader, headallocsize, 1, fptr);
+  printf("reaturned ret: %d\n", ret);
 
-  printf("char: %c\n", elfheader.magicnumber[3]);
+  if (headeris64) {
+    printf("Header is 64\n");
+    elfhead_t_64 *elfheader64 = (elfhead_t_64 *)elfheader;
+    printf("Elf file header:\n");
+    printf("magicnumber: %s\n", elfident.magicnumber);
+    printf("bitformat: %d\n", elfident.bitformat);
+    printf("endianness: %d\n", elfident.endianness);
+    printf("headersize1: %d\n", elfheader64->headersize);
+    printf("version1: %#x\n", elfheader64->version);
+    printf("char: %c\n", elfident.magicnumber[3]);
+  } else {
+    printf("Header is 32\n");
+    elfhead_t_32 *elfheader32 = (elfhead_t_32 *)elfheader;
+    printf("Elf file header:\n");
+    printf("magicnumber: %s\n", elfheader32->ident.magicnumber);
+    printf("bitformat: %d\n", elfheader32->ident.bitformat);
+    printf("endianness: %d\n", elfheader32->ident.endianness);
+    printf("char: %c\n", elfheader32->ident.magicnumber[3]);
+  }
 
+  free(elfheader);
   printf("Sandbox end\n");
 }
