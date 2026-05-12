9b3f8f5804af74800b7014715548b798d9c56167
Nadav Dan <nadav.prod@gmail.com>
2026-05-12 20:55:46 +0300

    added loader for 32 bitclass

--- src/elfloader.c
+++ src/elfloader.c
@@ -40,7 +40,7 @@ int load(FILE *fp, elf_generic_headers *elf) {
         void *mem = mmap((void *)seg_start, seg_size, prots,
                          MAP_FIXED | MAP_PRIVATE, fileno(fp), file_off);
         if (mem == MAP_FAILED) {
-          printf("ERROR: Failed mmap.");
+          printf("ERROR: Failed mmap.\n");
           return 1;
         }
 
@@ -49,9 +49,49 @@ int load(FILE *fp, elf_generic_headers *elf) {
           memset(mem + phdr.p_filesz + data_offset, 0,
                  phdr.p_memsz - phdr.p_filesz);
         }
+
+        printf("mem: %p\n", mem);
       }
     }
   } else if (elf->bit_class == ELFCLASS32) {
+    for (i = 0; i < elf->elf_header.h32.e_phnum; i++) {
+      Elf32_Phdr phdr = elf->program_headers.h32[i];
+      uint32_t prots = PROT_NONE;
+      if (phdr.p_type == PT_LOAD) {
+        uintptr_t seg_start = ALIGN_DOWN(phdr.p_vaddr, page_size);
+        uintptr_t seg_end = ALIGN_UP(phdr.p_vaddr + phdr.p_memsz, page_size);
+
+        size_t seg_size = seg_end - seg_start;
+
+        off_t file_off = ALIGN_DOWN(phdr.p_offset, page_size);
+
+        if (phdr.p_flags & PF_X) {
+          prots |= PROT_EXEC;
+        }
+        if (phdr.p_flags & PF_W) {
+          prots |= PROT_WRITE;
+        }
+        if (phdr.p_flags & PF_R) {
+          prots |= PROT_READ;
+        }
+        printf("virtural mem: seg_start: %zu, seg_size: %zu, file_off: %jd\n",
+               seg_start, seg_size, file_off);
+        void *mem = mmap((void *)seg_start, seg_size, prots,
+                         MAP_FIXED | MAP_PRIVATE, fileno(fp), file_off);
+        if (mem == MAP_FAILED) {
+          printf("ERROR: Failed mmap.\n");
+          return 1;
+        }
+
+        uintptr_t data_offset = phdr.p_vaddr - seg_start;
+        if (phdr.p_filesz < phdr.p_memsz) {
+          memset(mem + phdr.p_filesz + data_offset, 0,
+                 phdr.p_memsz - phdr.p_filesz);
+        }
+
+        printf("mem: %p\n", mem);
+      }
+    }
   } else {
     printf("ERROR: unknown bit_class: %d", elf->bit_class);
     return 1;
